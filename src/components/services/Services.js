3fdd6d0ce70de5b7b4b4d73ed802981b9fb9ca71
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-10-04 22:28:00 +0600

    d

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Row, Button } from 'react-bootstrap';
+import { Card, Col, Row, Button, Spinner } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import LoadData from '../loadData/LoadData';
 import './Services.css'
@@ -25,27 +25,39 @@ const Services = () => {
     return (
         <div className='container mt-5' style={{ color: '#2C3E50' }}>
             <h1 className='my-4 fw-bold text-center  display-5 text-uppercase'>Popular Services</h1>
-            <Row xs={1} md={4} className="g-4">
-                {data.slice(0, 4).map(department => (
-                    <Col>
-                        <Card>
-                            <div className="card-img">
-                                <Card.Img className='img' variant="top" src={department.photo} />
-                            </div>
-                            <Card.Body>
-                                <Card.Title className='text-center'>{department.dept} Technology</Card.Title>
-                                <Card.Text >
-                                    <div><i class="fas fa-dollar-sign"></i> {department.fee} Tk/Semester</div>
-                                    <div><i class="far fa-clock"></i> Duration: {department.duration}</div>
-                                </Card.Text>
-                            </Card.Body>
-                            <Card.Footer class="p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><Button style={button} class="mt-auto">View options</Button></div>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            {data.length === 0 ?
+                <div className="py-5 text-center">
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                </div>
+
+                :
+                <Row xs={1} md={4} className="g-4">
+                    {data.slice(0, 4).map(department => (
+                        <Col>
+                            <Card>
+                                <div className="card-img">
+                                    <Card.Img className='img' variant="top" src={department.photo} />
+                                </div>
+                                <Card.Body>
+                                    <Card.Title className='text-center'>{department.dept} Technology</Card.Title>
+                                    <Card.Text >
+                                        <div><i class="fas fa-dollar-sign"></i> {department.fee} Tk/Semester</div>
+                                        <div><i class="far fa-clock"></i> Duration: {department.duration}</div>
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer class="p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><Button style={button} class="mt-auto">View options</Button></div>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>}
             <Button variant="primary" size="md" className='fw-bold mt-4' style={button}>
                 <NavLink activeStyle={activeactiveStyle} style={navbar} to="/AllServices">
                     View More  <i class="ms-2 fas fa-arrow-right"></i>
