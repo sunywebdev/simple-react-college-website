853ce5cee4bd85c66176a76ba1a48ad5aacce801
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-10-04 22:07:52 +0600

    s

--- src/components/allServices/Allservices.js
+++ src/components/allServices/Allservices.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Row, Button } from 'react-bootstrap';
+import { Card, Col, Row, Button, Spinner } from 'react-bootstrap';
 import '../services/Services.css'
 import LoadData from '../loadData/LoadData';
 
@@ -13,29 +13,42 @@ const Allservices = () => {
     const [data] = LoadData()
 
     return (
-        <div className='container my-5' style={{ color: '#2C3E50' }}>
-            <h1 className='my-5 fw-bold text-center'>Services</h1>
-            <Row xs={1} md={4} className="g-4">
-                {data.map(department => (
-                    <Col>
-                        <Card>
-                            <div className="card-img">
-                                <Card.Img className='img' variant="top" src={department.photo} />
-                            </div>
-                            <Card.Body>
-                                <Card.Title className='text-center'>{department.dept} Technology</Card.Title>
-                                <Card.Text >
-                                    <div><i class="fas fa-dollar-sign"></i> {department.fee} Tk/Semester</div>
-                                    <div><i class="far fa-clock"></i> Duration: {department.duration}</div>
-                                </Card.Text>
-                            </Card.Body>
-                            <Card.Footer class="p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><Button style={button} class="mt-auto">View options</Button></div>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+        <div className='container my-5'>
+            <h1 className='my-5 fw-bold text-center' style={{ color: '#2C3E50' }}>Services</h1>
+            {data.length === 0 ?
+                <div className="py-5 text-center">
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                    <Spinner animation="border" className='loading mx-2' />
+                </div>
+
+                :
+
+                <Row xs={1} md={4} className="g-4">
+                    {data.map(department => (
+                        <Col>
+                            <Card>
+                                <div className="card-img">
+                                    <Card.Img className='img' variant="top" src={department.photo} />
+                                </div>
+                                <Card.Body>
+                                    <Card.Title className='text-center'>{department.dept} Technology</Card.Title>
+                                    <Card.Text >
+                                        <div><i class="fas fa-dollar-sign"></i> {department.fee} Tk/Semester</div>
+                                        <div><i class="far fa-clock"></i> Duration: {department.duration}</div>
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer class="p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><Button style={button} class="mt-auto">View options</Button></div>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>}
         </div>
     );
 };
